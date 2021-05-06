934c5fbaa227f5cbb0cafde025f321a0d8faec2e
Elvir9 <ehalilovicc@gmail.com>
2021-05-07 01:21:53 +0200

    api for match documents in date range

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { deleteList, editList, getList, shoppingList } from '../controllers/list';
+import { aggregateList, deleteList, editList, getList, shoppingList } from '../controllers/list';
 import { requireAuth } from '../middlewares/authJwt';
 
 const router = Router();
@@ -8,5 +8,5 @@ router.post('/create-list', requireAuth, shoppingList);
 router.delete('/delete/:id', requireAuth, deleteList)
 router.get('/list/:userId', requireAuth, getList);
 router.patch('/edit/:id', requireAuth, editList);
-
+router.get('/aggregate', aggregateList);
 export default router;
